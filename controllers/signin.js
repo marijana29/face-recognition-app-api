9d8ad678f1995cbb23598de3e4c280865a2a6fe3
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-06 20:12:44 +0200

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,5 @@
 
-  const handleSignin = (req, res, db, bcrypt) => {
+  const signinHandler = (req, res, db, bcrypt) => {
     const { email, password } = req.body;
     if (!email || !password) {
   return res.status(400).json('incorrect form submission');
