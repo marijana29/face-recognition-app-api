41ac3b7d8f801e3128df61c6f8fa36627e5284e7
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-07 13:56:37 +0200

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -7,14 +7,20 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
   app.models
     .predict('face-detection', req.body.input)
-    .then(data => {
-      res.json(data);
+    .then((data) => {
+      if (data && data.outputs && data.outputs.length > 0) {
+        // Process the data as needed
+        res.json(data);
+      } else {
+        console.error('Invalid or empty response from Clarifai API:', data);
+        res.status(400).json('Unable to work with API');
+      }
     })
-    .catch(err => {
-      console.error(err); // Log the error for debugging
+    .catch((err) => {
+      console.error('Error calling Clarifai API:', err);
       res.status(400).json('Unable to work with API');
     });
-}
+};
 
 const imageHandler = (req, res, db) => {
   const { id, imageUrl } = req.body;
