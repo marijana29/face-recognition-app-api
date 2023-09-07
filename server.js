b6cfb1ba336aa2541fdd3257d57b8006a67a828d
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-07 09:05:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
 
 
-
-
-
-app.listen(5000, () => {
-  console.log('app is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
 });
+
+
