79d4d2ae9813205d95a82ef52e1aa95863a06b0e
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-07 10:45:50 +0200

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,32 +1,35 @@
 import Clarifai from 'clarifai';
 
-
 const app = new Clarifai.App({
   apiKey: process.env.API_CLARIFAI,
 });
 
 const handleApiCall = (req, res) => {
   app.models
-   .predict('face-detection', req.body.input)
-   .then(data => {
-    res.json(data)
-   })
-   .catch(err => res.status(400).json('unable to work with API'))
+    .predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.error(err); // Log the error for debugging
+      res.status(400).json('Unable to work with API');
+    });
 }
 
-
 const imageHandler = (req, res, db) => {
-  
   const { id, imageUrl } = req.body;
 
-  
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0].entries);
+    })
+    .catch(err => {
+      console.error(err); // Log the error for debugging
+      res.status(400).json('Unable to get entries');
+    });
 }
 
 export default imageHandler;
