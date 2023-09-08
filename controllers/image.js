cc589fb4e5ffc09fb671939c9e81fd06b234e654
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-08 09:10:35 +0200

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -25,17 +25,21 @@ const handleApiCall = (req, res) => {
 const imageHandler = (req, res, db) => {
   const { id, imageUrl } = req.body;
 
+  console.log('Received request to update entries for user ID:', id);
+
   db('users')
     .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
+      console.log('Updated entries:', entries[0].entries);
       res.json(entries[0].entries);
     })
     .catch(err => {
-      console.error(err); // Log the error for debugging
-      res.status(400).json('Unable to get entries');
+      console.error('Error updating entries:', err); // Log the error for debugging
+      res.status(400).json('Unable to update entries');
     });
 }
+
 
 export default { imageHandler, handleApiCall };
