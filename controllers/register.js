9088c9420d43d66d04d3eb21148e3bb0ae88d375
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-11 11:52:30 +0200

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt-nodejs';
 
-const handleRegister = (req, res, db) => {
+const registerHandler = (req, res, db) => {
   const { email, name, password } = req.body;
-  
+
   if (!email || !name || !password) {
     return res.status(400).json('Incorrect form submission');
   }
@@ -34,4 +34,4 @@ const handleRegister = (req, res, db) => {
   .catch(err => res.status(400).json('Unable to register'));
 };
 
-export default { handleRegister };
+export default { registerHandler };
