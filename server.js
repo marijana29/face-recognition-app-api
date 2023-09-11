86b728d2ab59d169345da3aed073462e47fc4692
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-11 20:56:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get('/', (req, res)=> { res.send(db.users) });
 app.post('/signin', (req, res) => { signinHandler(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { registerHandler(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profileHandler(req, res, db) });
-app.put('/image', (req, res) => { imageHandler.handleApiCall(req, res, db) });
-app.post('/imageurl', (req, res) => { imageHandler.handleApiCall(req, res) });
+app.put('/image', (req, res) => { image.imageHandler(req, res, db) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
