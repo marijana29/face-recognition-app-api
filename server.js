8598b04c8d79c290d431baff9300f6ddaa3d37ac
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-11 21:50:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
 
-
 import registerHandler from "./controllers/register.js";
 import signinHandler from "./controllers/signin.js";
 import profileHandler from "./controllers/profile.js";
-import imageHandler from "./controllers/image.js";
+import image from "./controllers/image.js"; // Use import instead of { imageHandler, handleApiCall }
 
 const db = knex({
-client: 'pg',
-connection: {
-connectionString : process.env.DATABASE_URL,
-ssl: { rejectUnauthorized: false },
-host: process.env.DATABASE_HOST,
-port: 5432,
-user: process.env.DATABASE_USER,
-password: process.env.DATABASE_PW,
-database: process.env.DATABASE_DB
-}
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
+  }
 });
 
 const app = express();
@@ -28,12 +27,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send(db.users) });
+app.get('/', (req, res) => { res.send(db.users) });
 app.post('/signin', (req, res) => { signinHandler(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { registerHandler(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profileHandler(req, res, db) });
-app.put('/image', (req, res) => { image.imageHandler(req, res, db) });
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+app.put('/image', (req, res) => { image.imageHandler(req, res, db) }); // Use 'image' instead of { imageHandler }
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) }); // Use 'image' instead of { handleApiCall }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
