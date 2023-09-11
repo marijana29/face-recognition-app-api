c9030152282497de3009bc6630efc3090b417a6a
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-11 11:27:01 +0200

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -4,42 +4,32 @@ const app = new Clarifai.App({
   apiKey: process.env.API_CLARIFAI,
 });
 
-const handleApiCall = (req, res, db) => {
+const handleApiCall = (req, res) => {
   app.models
     .predict('face-detection', req.body.input)
-    .then((data) => {
-      if (data && data.outputs && data.outputs.length > 0) {
-        // Process the data as needed
-        res.json(data);
-      } else {
-        console.error('Invalid or empty response from Clarifai API:', data);
-        res.status(400).json('Unable to work with API');
-      }
+    .then(data => {
+      res.json(data);
     })
-    .catch((err) => {
-      console.error('Error calling Clarifai API:', err);
+    .catch(err => {
+      console.error(err); // Log the error for debugging
       res.status(400).json('Unable to work with API');
     });
-};
+}
 
-const imageHandler = (req, res) => {
+const imageHandler = (req, res, db) => {
   const { id, imageUrl } = req.body;
-
-  console.log('Received request to update entries for user ID:', id);
 
   db('users')
     .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
-      console.log('Updated entries:', entries[0].entries);
       res.json(entries[0].entries);
     })
     .catch(err => {
-      console.error('Error updating entries:', err); // Log the error for debugging
-      res.status(400).json('Unable to update entries');
+      console.error(err); // Log the error for debugging
+      res.status(400).json('Unable to get entries');
     });
 }
-
 
 export default { imageHandler, handleApiCall };
